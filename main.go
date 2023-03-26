3b4b33c9fe2eff28c2d404a37efdb54dab5fe252
pgiri <parthasarathi.giri@nokia.com>
2023-03-26 20:21:25 +0530

    Adding RestGet api call

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+func getApiCall(v string, wg *sync.WaitGroup) {
+	resp, _ := http.Get(v)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	wg.Done()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	url := []string{
+		"https://httpbin.org/get",
+		"http://api.github.com/users/hadley/orgs",
+	}
+
+	for _, v := range url {
+		wg.Add(1)
+		go getApiCall(v, &wg)
+	}
+	wg.Wait()
+}
